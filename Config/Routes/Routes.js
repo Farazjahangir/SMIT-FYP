a789994bbf3e0755edcf9555d47399cb02b246d2
farazjahangir <jahangirfaraz98@gmail.com>
2019-02-22 08:45:10 -0800

    Drawer Navigator implemented

--- Config/Routes/Routes.js
+++ Config/Routes/Routes.js
@@ -1,21 +1,36 @@
-import { createStackNavigator, createAppContainer } from "react-navigation";
+import { createStackNavigator, createAppContainer, createDrawerNavigator } from "react-navigation";
 import Login from '../../Screens/Login/Login'
 import SavingProfile from '../../Screens/SavingProfile/SavingProfile'
 import Dashboard from '../../Screens/Dashboard/Dashboard'
 import SubmitSkills from '../../Screens/SubmitSkills/SubmitSkills'
-const AppNavigator = createStackNavigator({
-    // Login: {
-    //     screen: Login
-    // },
-    // SavingProfile: {
-    //     screen: SavingProfile
-    // },
-    // Dashboard : {
-    //     screen : Dashboard
-    // },
+
+
+
+
+const DrawerNavigator = createDrawerNavigator({
+    Dashboard: {
+        screen: Dashboard,
+    },
     SubmitSkills : {
         screen : SubmitSkills
     }
 });
 
-export default createAppContainer(AppNavigator);
+const AppNavigator = createStackNavigator({
+    // Login: {
+    //     screen: Login
+    // },
+    SavingProfile: {
+        screen: SavingProfile
+    },
+    Dashboard: {
+        screen: DrawerNavigator,
+    },
+    SubmitSkills: {
+        screen: SubmitSkills
+    }
+});
+
+const Navigator = createAppContainer(AppNavigator)
+
+export default Navigator

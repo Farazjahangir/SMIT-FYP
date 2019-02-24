2da4c0df649d36a9874d0f039f677ed97b74d2e3
farazjahangir <jahangirfaraz98@gmail.com>
2019-02-24 04:48:34 -0800

    validation

--- Screens/SavingProfile/SavingProfile.js
+++ Screens/SavingProfile/SavingProfile.js
@@ -11,11 +11,12 @@ class SavingProfile extends React.Component {
 
     this.state = {
       picUrl: false,
-      userName: '',
-      userUid : '',
+      userName: 'asas',
+      userUid : 'sa',
       profilePic : '',
       nextStep: false,
-      contactNum : ''
+      contactNum : '',
+      error : false
     }
   }
   static navigationOptions = {
@@ -30,65 +31,94 @@ class SavingProfile extends React.Component {
     this.setState({ nextStep: true })
   }
   savingDataToFirebase(){
-    const { userName , profilePic , userUid , contactNum } =  this.state
+    const { userName , profilePic , userUid , contactNum, error } =  this.state
+    if(userName || contactNum === ""){
+      this.setState({error : true})
+      return
+    }
+    this.setState({error : false})
     const userObj = {
       userName,
       profilePic,
       contactNum,
       userUid
     }
-    savingUserData(userObj).then((result)=>{console.log(result);
+    savingUserData(userObj).then((result)=>{this.props.navigation.replace('Dashboard');
     })
   }
 
   render() {
-    const { userName, profilePic, userUid, picUrl, nextStep, contactNum } = this.state
+    const { userName, profilePic, userUid, picUrl, nextStep, contactNum, error } = this.state
     return (
-      <View style={{ flex: 1 }}>
-        {!nextStep &&
-          <View style={{ flex: 1 }}>
-            <View style={styles.container}>
-              <Image
-                source={picUrl ? {uri : profilePic} :  require('../../images/dummyPic.png')}
-                style={styles.profilePic}
-              />
-              {/* <TouchableOpacity style={styles.uploadPicBtn} onPress={() => { this.changePic() }} >
-                <Text style={styles.uploadPicBtnText}>Change Your Picture</Text>
-              </TouchableOpacity> */}
-            </View>
-            <View style={{ flex: 0.30, alignItems: 'flex-end' }}>
-              <TouchableOpacity style={styles.nextPicBtn} onPress={() => { this.nextStep() }}>
-                <Text style={styles.nextPicBtnText}>Next</Text>
-              </TouchableOpacity>
+      // <View style={{ flex: 1 }}>
+      //     <View style={{ flex: 1 }}>
+      //       <View style={styles.container}>
+      //         <Image
+      //           source={picUrl ? {uri : profilePic} :  require('../../images/dummyPic.png')}
+      //           style={styles.profilePic}
+      //         />
+      //         {/* <TouchableOpacity style={styles.uploadPicBtn} onPress={() => { this.changePic() }} >
+      //           <Text style={styles.uploadPicBtnText}>Change Your Picture</Text>
+      //         </TouchableOpacity> */}
+      //       </View>
+      //       <View style={{ flex: 0.30, alignItems: 'flex-end' }}>
+      //         <TouchableOpacity style={styles.nextPicBtn} onPress={() => { this.nextStep() }}>
+      //           <Text style={styles.nextPicBtnText}>Next</Text>
+      //         </TouchableOpacity>
 
-            </View>
+      //       </View>
+      //     </View>
+      //     <View style={{flex : 1}}>
+      //       <View style={{ flex: 0.35, marginTop: '25%' }}>
+      //         <TextInput
+      //           style={styles.input}
+      //           placeholder='Enter Your Name'
+      //           placeholderTextColor='#535c68'
+      //           defaultValue={userName}
+      //           onChange = {(e)=>{this.setState({userName : e.nativeEvent.text})}}
+      //         />
+      //         <TextInput
+      //           style={styles.input}
+      //           onChange = {(e)=>{this.setState({contactNum : e.nativeEvent.text})}}
+      //           placeholder='Enter Your Phone Number'
+      //           placeholderTextColor='#535c68'
+      //           keyboardType='number-pad'
+      //         />
+      //       </View>
+      //       <View style={{ flex: 0.30, alignItems: 'flex-end', marginRight: 10 }}>
+      //         <TouchableOpacity style={styles.nextPicBtn} onPress = {()=>{this.savingDataToFirebase()}}>
+      //           <Text style={styles.nextPicBtnText}>Finish</Text>
+      //         </TouchableOpacity>
+      //       </View>
+      //         <View style={{ alignItems : 'center'}}>
+      //           <Text style={{color : 'red'  , fontSize : 18}}>All Fields Are Required</Text>
+      //         </View>
+      //     </View>
+      // </View>
+      <View style={styles.container}>
+          <Image
+            source={profilePic ? {uri : profilePic} :  require('../../images/dummyPic.png')}
+            style={styles.profilePic}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder='Enter Your Name'
+            placeholderTextColor='#535c68'
+            defaultValue={userName}
+            onChange = {(e)=>{this.setState({userName : e.nativeEvent.text})}}
+          />
+          <TextInput
+            style={styles.input}
+            onChange = {(e)=>{this.setState({contactNum : e.nativeEvent.text})}}
+            placeholder='Enter Your Phone Number'
+            placeholderTextColor='#2C3A47'
+            keyboardType='number-pad'
+          />
+          <View style={{alignItems : 'flex-end'}}>
+            <TouchableOpacity style={styles.nextPicBtn} onPress = {()=>{this.savingDataToFirebase()}}>
+            <Text style={styles.nextPicBtnText}>Finish</Text>
+            </TouchableOpacity>
           </View>
-        }
-        {nextStep &&
-          <View style={{flex : 1}}>
-            <View style={{ flex: 0.35, marginTop: '25%' }}>
-              <TextInput
-                style={styles.input}
-                placeholder='Enter Your Name'
-                placeholderTextColor='#535c68'
-                defaultValue={userName}
-                onChange = {(e)=>{this.setState({userName : e.nativeEvent.text})}}
-              />
-              <TextInput
-                style={styles.input}
-                onChange = {(e)=>{this.setState({contactNum : e.nativeEvent.text})}}
-                placeholder='Enter Your Phone Number'
-                placeholderTextColor='#535c68'
-                keyboardType='number-pad'
-              />
-            </View>
-            <View style={{ flex: 0.30, alignItems: 'flex-end', marginRight: 10 }}>
-              <TouchableOpacity style={styles.nextPicBtn} onPress = {()=>{this.savingDataToFirebase()}}>
-                <Text style={styles.nextPicBtnText}>Finish</Text>
-              </TouchableOpacity>
-            </View>
-          </View>
-        }
       </View>
     );
   }
@@ -100,57 +130,53 @@ const mapDispatchToProps = (dispatch) => {
 }
 const mapStateToProps = (state) => {
   return {
-    userName: state.authReducer.user.userName,
-    profilePic : state.authReducer.user.profilePic,
-    userUid : state.authReducer.user.userUid
+    // userName: state.authReducer.user.userName,
+    // profilePic : state.authReducer.user.profilePic,
+    // userUid : state.authReducer.user.userUid
   }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(SavingProfile)
 const styles = StyleSheet.create({
   container: {
-    flex: 0.50,
-    backgroundColor: '#fff',
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    marginTop: 80
   },
-  uploadPicBtn: {
-    alignItems: 'center',
-    backgroundColor: '#eb2f06',
-    padding: 10,
-    marginTop: 10
-  },
-  uploadPicBtnText: {
-    color: '#fff',
-    fontSize: 20
-  },
+  // uploadPicBtn: {
+  //   alignItems: 'center',
+  //   backgroundColor: '#eb2f06',
+  //   padding: 10,
+  //   marginTop: 10
+  // },
+  // uploadPicBtnText: {
+  //   color: '#fff',
+  //   fontSize: 20
+  // },
   nextPicBtn: {
     backgroundColor: '#1e3799',
     padding: 15,
     marginTop: 15,
-    marginRight: 25,
-    width: 100,
+    width: 120,
     alignItems: 'center'
   },
   nextPicBtnText: {
     color: '#fff',
-    fontSize: 20,
-    fontSize: 18,
+    fontSize: 25,
   },
   profilePic: {
     width: 200,
     height: 200,
-    borderRadius: 100
+    borderRadius: 100,
+    marginBottom : 30
   },
   input: {
-    width: '90%',
-    borderWidth: 2,
-    borderColor: '#CAD3C8',
-    padding: 6,
-    marginTop: 20,
-    padding: 10,
-    marginLeft: 10
+    width: '80%',
+    borderRadius: 10,
+    borderWidth : 3,
+    borderColor: '#2C3A47',
+    padding: 8,
+    marginTop: 10,
   }
 
 });

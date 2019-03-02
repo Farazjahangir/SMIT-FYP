3427586c4c98e199551eb1919815b9edc8218763
farazjahangir <jahangirfaraz98@gmail.com>
2019-03-01 16:31:59 -0800

    modal created for full details of skill

--- Screens/Dashboard/Dashboard.js
+++ Screens/Dashboard/Dashboard.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux'
 
-import { StyleSheet, ScrollView, FlatList, View, Image, TouchableOpacity, TextInput, Button } from 'react-native';
-import { Content, List, ListItem, Left, Body, Right, Thumbnail, Text, Spinner } from 'native-base';
+import { StyleSheet, ScrollView, View, Modal } from 'react-native';
+import { Content, List, ListItem, Left, Body, Right, Thumbnail, Text, Spinner, Card, CardItem } from 'native-base';
 import CustomButton from '../../Components/CustomButton/CustomButton'
 import { firebase } from '../../Config/Firebase/Firebase'
 import CustomHeader from '../../Components/CustomHeader/CustomHeader'
@@ -13,7 +13,9 @@ class Dashboard extends React.Component {
             showSideBar: false,
             skillsArr: [],
             list: false,
-            isLoading: true
+            isLoading: true,
+            modalVisible: false,
+            index: 0
         }
     }
 
@@ -44,9 +46,20 @@ class Dashboard extends React.Component {
             });
     }
 
+    // getFullDetails(i){
+    //     const { skillsArr } = this.state
+    //     console.log('getFullDetails' , skillsArr[i]);
+
+    // }
+
     render() {
-        const { showSideBar, skillsArr, list, isLoading } = this.state
-        //    console.log('State' , this.state.skillsArr);
+        const { skillsArr, 
+                list, 
+                isLoading, 
+                modalVisible, 
+                index 
+            } = this.state
+        console.log('Detal', skillsArr[index]);
 
         return (
             <View style={{ flex: 1 }}>
@@ -54,43 +67,88 @@ class Dashboard extends React.Component {
                 {isLoading && <Spinner color='blue' style={{ flex: 1, justifyContent: 'center' }} />}
                 <ScrollView vertical={true} contentContainerStyle={styles.contentContainer}>
                     {list &&
-                        skillsArr.map((val, i) => {
-                            return <Content key={Date.now() + i} style={{ marginTop: 20 }}>
-                                <List >
-                                    <ListItem avatar>
-                                        <Left>
-                                            <Thumbnail source={{ uri: val.picUrl }} />
-                                        </Left>
-                                        <Body>
-                                            <Text>{val.SkillName}</Text>
-                                            <Text note numberOfLines={2}>{val.Description}</Text>
-                                        </Body>
-                                        <Right>
-                                            <CustomButton 
-                                                title={'View'}
-                                                style={[styles.viewBtn, styles.viewBtnText]}
+                        <View>
+                            <Modal
+                                animationType="slide"
+                                visible={modalVisible}
+                                onRequestClose={() => { this.setState({ modalVisible: false }) }}
+                            >
+                                <ScrollView vertical={true}>
+                                    <View style={{ marginTop: 40, marginLeft: 15 }}>
+                                        <Text style={{fontSize : 30 , textAlign : 'center'}}>Details</Text>
+                                        <Card>
+                                            <CardItem header bordered>
+                                                <Text>Skill</Text>
+                                            </CardItem>
+                                            <CardItem bordered>
+                                                <Body>
+                                                    <Text>{skillsArr[index].SkillName}</Text>
+                                                </Body>
+                                            </CardItem>
+                                            <CardItem header bordered>
+                                                <Text>Dexcription</Text>
+                                            </CardItem>
+                                            <CardItem bordered>
+                                                <Body>
+                                                    <Text>{skillsArr[index].Description}</Text>
+                                                </Body>
+                                            </CardItem>
+                                            <CardItem header bordered>
+                                                <Text>Charges</Text>
+                                            </CardItem>
+                                            <CardItem bordered>
+                                                <Body>
+                                                    <Text>{skillsArr[index].Rate}</Text>
+                                                </Body>
+                                            </CardItem>
+                                        </Card>
+                                        <View style={{ flexDirection: 'row', marginTop: 20 }}>
+                                            <CustomButton
+                                                title={'Contact'}
+                                                onPress={() => { this.setState({ modalVisible: false }) }}
+                                                style={[styles.contactBtn, styles.contactBtnText]}
                                             />
-                                        </Right>
-                                    </ListItem>
-                                </List>
-                            </Content>
+                                            <CustomButton
+                                                title={'Send Proposal'}
+                                                onPress={() => { this.setState({ modalVisible: false }) }}
+                                                style={[styles.proposalBtn, styles.proposalBtnText]}
+                                            />
+                                        </View>
+                                        <View style={{ alignItems: 'center' }}>
+                                            <CustomButton
+                                                title={'Back'}
+                                                onPress={() => { this.setState({ modalVisible: false }) }}
+                                                style={[styles.modalBackBtn, styles.modalBackBtnText]}
+                                            />
+                                        </View>
+                                    </View>
+                                </ScrollView>
+                            </Modal>
+                            {skillsArr.map((val, i) => {
+                                return <Content key={Date.now() + i} style={{ marginTop: 20 }}>
+                                    <List >
+                                        <ListItem avatar>
+                                            <Left>
+                                                <Thumbnail source={{ uri: val.picUrl }} />
+                                            </Left>
+                                            <Body>
+                                                <Text>{val.SkillName}</Text>
+                                                <Text note numberOfLines={2}>{val.Description}</Text>
+                                            </Body>
+                                            <Right>
+                                                <CustomButton
+                                                    title={'View'}
+                                                    style={[styles.viewBtn, styles.viewBtnText]}
+                                                    onPress={() => { this.setState({ modalVisible: true, index: i }) }}
+                                                />
+                                            </Right>
+                                        </ListItem>
+                                    </List>
+                                </Content>
 
-                        })
+                            })}
+                        </View>
                     }
-                    {/* <List>
-                        <ListItem avatar>
-                        <Left>
-                            <Thumbnail source={require('../../images/download.png')} />
-                        </Left>
-                        <Body>
-                            <Text>Kumar Pratik</Text>
-                            <Text note numberOfLines = {2}>Doing what you like will always keep you happy. .</Text>
-                        </Body>
-                        <Right>
-                            <Button style={[styles.viewBtn , styles.viewBtnText]}>View</Button>
-                        </Right>
-                        </ListItem>
-          </List> */}
                 </ScrollView>
             </View>
         )
@@ -131,5 +189,48 @@ const styles = StyleSheet.create({
     viewBtnText: {
         color: '#fff',
         fontSize: 13
+    },
+    modalBackBtn: {
+        borderColor: '#0984e3',
+        borderWidth: 2,
+        backgroundColor: '#0984e3',
+        padding: 5,
+        borderRadius: 20,
+        marginTop: 30,
+        width: 200,
+        alignItems: 'center',
+        marginBottom : 20
+    },
+    modalBackBtnText: {
+        color: '#fff',
+        fontSize: 17
+    },
+    contactBtn: {
+        borderColor: '#0984e3',
+        borderWidth: 2,
+        backgroundColor: '#0984e3',
+        padding: 5,
+        borderRadius: 10,
+        width: 110,
+        alignItems: 'center'
+    },
+    contactBtnText: {
+        color: '#fff',
+        fontSize: 16
+    },
+    proposalBtn: {
+        borderColor: '#0984e3',
+        borderWidth: 2,
+        backgroundColor: '#0984e3',
+        padding: 5,
+        borderRadius: 10,
+        marginLeft: 20,
+        width: 150,
+        alignItems: 'center'
+    },
+    proposalBtnText: {
+        color: '#fff',
+        fontSize: 16
     }
+
 });
